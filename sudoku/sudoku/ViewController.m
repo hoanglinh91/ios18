823972f548ad71dc54f25fe0b5cb8a724124e908
hoanglinh91 <hoanglinh91@gmail.com>
2013-10-09 01:48:16 +0700

    adding data : sudoku.txt

--- sudoku/sudoku/ViewController.m
+++ sudoku/sudoku/ViewController.m
@@ -19,28 +19,52 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    int x = 100,y = 50,space = 5, height = 20,width = 20;
+    int x = 100,y = 50,space = 5, height = 20,width = 20, buttonPerRow = 9;
     
     for (int i=0; i<9; i++) {
         for (int j=0; j<9; j++) {
+            
+            // creat a button
             UIButton *myButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+            
+            // init button
             [myButton setTitle:@"x" forState:UIControlStateNormal];
             [myButton setFrame:CGRectMake(y, x, width , height)];
-            myButton.tag = i * 9 + j + 1;
+            
+            // add tag for button
+            myButton.tag = i * buttonPerRow + j + 1;
+            
+            // new y
             y += height;
             y += space;
+            // add button to view
             [self.view addSubview:myButton];
+            // event for button
             [myButton addTarget:self action:@selector(myButtonIsPressed:) forControlEvents:UIControlEventTouchUpInside];
         }
+        // new x
         x += height;
         x += space;
+        // init y
         y = 50;
     }
 }
 
 -(void)myButtonIsPressed:(UIButton *) button{
+    
+    // test button pressed
     NSLog(@"%d",button.tag);
+    
     [button setTitle:[[NSString alloc] initWithFormat:@"%d",button.tag]  forState:UIControlStateNormal];
+
+    // test tag detect
+    if (button.tag == 81) {
+        for (UIButton * myButton in self.view.subviews) {
+            if ([myButton isKindOfClass:[UIButton class]]) {
+                NSLog(@"%@",myButton.titleLabel.text);
+            }
+        }
+    }
 }
 
 - (void)didReceiveMemoryWarning
